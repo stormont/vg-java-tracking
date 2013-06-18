03587478057525cd778d49a35dd292be2464f929
Devan Stormont <stormont@gmail.com>
2013-06-17 22:07:00 -0700

    Added a Versioning class to help with JAR/API management.

---
+++ vg-java-tracking/src/com/voyagegames/java/tracking/Versioning.java
@@ -0,0 +1,21 @@
+package com.voyagegames.java.tracking;
+
+/**
+ * Versioning for the vg-java-tracking library
+ */
+public class Versioning {
+	
+	/**
+	 * A value count for the revision level of the library
+	 */
+	public static final int REVISION = 1;
+	/**
+	 * The major version level of the API (differences indicate incompatible interface changes)
+	 */
+	public static final int MAJOR = 1;
+	/**
+	 * The minor version level of the API (differences indicate compatible interface changes)
+	 */
+	public static final int MINOR = 0;
+
+}
